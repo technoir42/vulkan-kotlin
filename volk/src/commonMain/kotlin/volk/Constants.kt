91a8adc069d23614aa85cd53fbd7c8e5e80c7bc7
technoir <technoir.dev@gmail.com>
2025-08-05 22:46:59 +0100

    Define VK_FALSE and VK_TRUE

--- volk/src/commonMain/kotlin/volk/Constants.kt
+++ volk/src/commonMain/kotlin/volk/Constants.kt
@@ -5,3 +5,6 @@ val VK_API_VERSION_1_1: UInt = VK_MAKE_API_VERSION(0u, 1u, 1u, 0u)
 val VK_API_VERSION_1_2: UInt = VK_MAKE_API_VERSION(0u, 1u, 2u, 0u)
 val VK_API_VERSION_1_3: UInt = VK_MAKE_API_VERSION(0u, 1u, 3u, 0u)
 val VK_API_VERSION_1_4: UInt = VK_MAKE_API_VERSION(0u, 1u, 4u, 0u)
+
+const val VK_FALSE = 0u
+const val VK_TRUE = 1u
